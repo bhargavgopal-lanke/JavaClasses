c7a29bae4084951ee8d1cc078a0f18186dbef629
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-11-13 14:16:45 +0530

    first commit

---
+++ Authentication.java
@@ -0,0 +1,23 @@
+public class Authentication { // class
+
+    public static void main() {
+        // System.out.println("main is the main method in class to execute other methods");
+        Authentication createObj = new Authentication();
+        createObj.Login();
+        createObj.CreateAccount();
+        createObj.ResetPassword();
+
+    }
+
+    public void Login() { // method
+        System.out.println("Logged in");
+    }
+
+    public void CreateAccount() {
+        System.out.println("Account Created");
+    }
+
+    public void ResetPassword() {
+        System.out.println("Password reset");
+    }
+}
---
+++ Courses.java
@@ -0,0 +1,20 @@
+public class Courses {
+    public static void main() {
+        Courses createObj = new Courses();
+        createObj.GetCoursesList();
+        createObj.GetSingleCourseDetails();
+        createObj.BuyCourse();
+    }
+
+    public void GetCoursesList() {
+        System.out.println("get Courses list");
+    }
+
+    public void GetSingleCourseDetails() {
+        System.out.println("Get Single course details");
+    }
+
+    public void BuyCourse() {
+        System.out.println("Buy a course");
+    }
+}
