38802ce8cdbe86b4045fdcefb8f84b95b583c559
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-11-15 12:49:57 +0530

    Add Variables class with main method and basic variable declarations

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int noOfLikes = 100;
+        int noOfDislikes = 10;
+        float priceOfItems = 250;
+        char singleChar = 'B';
+        boolean isLiked = true;
+        boolean isDisliked = false;
+        System.out.println(noOfDislikes);
+        System.out.println(noOfLikes);
+    }
+}
