42ac35754b9209ee97838a4ead67dc14b4baec15
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-11-16 15:00:39 +0530

    Implement basic functionality for NumberVariables class to calculate total price

--- NumberVariables.java
+++ NumberVariables.java
@@ -1,5 +1,10 @@
 public class NumberVariables {
     public static void main(String[] args) {
-        
+        String price = "199.9";
+        String quantity = "2";
+        float totalPrice = Float.parseFloat(price) * Integer.parseInt(quantity);
+        System.out.println(totalPrice);
+        String outputMsg = "This is final Price " + totalPrice;
+        System.out.println(outputMsg);
     }
 }
