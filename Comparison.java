9206512a05a3e1c2189c0946276e43d0f3333449
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-12-04 13:58:16 +0530

    Fix variable naming and comparison logic in Comparison class

--- Comparison.java
+++ Comparison.java
@@ -5,12 +5,12 @@ public class Comparison {
         String dbEmail = "contact@gmail.com";
         String dbpassword = "12345678";
         String userEmail = "contact@gmail.com";
-        String password = "12345678";
+        String userpassword = "12345678";
         int loggedinUserId = 8;
         System.out.println(loggedinUserId != adminId);
 
         System.out.println(userEmail == dbEmail);
-        System.out.println(dbpassword == password);
+        System.out.println(dbpassword == userpassword);
         System.out.println(adminId == userId);
 
         int billTotal = 300;
@@ -20,6 +20,11 @@ public class Comparison {
         System.out.println(billTotal < minAmount);
         System.out.println(billTotal <= minAmount);
         System.out.println(billTotal >= minAmount);
+
+        // logical operators
+
+        Boolean UserDetails = userEmail == dbEmail && dbpassword == userpassword;
+        System.out.println("UserDetails: " + UserDetails);
 
     }
 }
