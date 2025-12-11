96df3696fe208bdd6bc148c56ac18cfc6ce9ec1c
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-12-11 10:43:02 +0530

    minor change

--- Payments.java
+++ Payments.java
@@ -3,14 +3,14 @@ public class Payments {
 
     public void CreateOrderId() {
         orderId = 25;
-        System.out.println("create order id" + orderId);
+        System.out.println("create order id " + orderId);
     }
 
     public void Refund() {
-        System.out.println("Refund amount" + orderId);
+        System.out.println("Refund amount " + orderId);
     }
 
     public void CancelOrder() {
-        System.out.println("cancel order id" + orderId);
+        System.out.println("cancel order id " + orderId);
     }
 }
