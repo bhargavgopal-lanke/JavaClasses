d70dc856e503129641df08ea55c2bbabe5110175
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-07-03 08:01:04 +0530

    Refactor access control logic in AndorNot class using switch statement

--- AndorNot.java
+++ AndorNot.java
@@ -2,19 +2,40 @@ public class AndorNot {
     public static void main(String[] args) {
         int adminId = 20;
         int userId = 15;
-        int editorId = 20;
+        int editorId = 21;
+        int viewId = 22;
 
-        if(adminId == userId) {
-            System.out.println("Welcome Admin " + userId);
+        // if(adminId == userId) {
+        //     System.out.println("Welcome Admin " + userId);
+        // };
+
+        // if(editorId == userId) {
+        //     System.out.println("Welcome Editor");
+        // };
+
+        // if(editorId == userId || adminId == userId) {
+        //     System.out.println("You can edit the files");
+        // } else if(viewId == userId) {
+        //     System.out.println("You can  view the files");
+        // } else {
+        //     System.out.println("You dont have access");
+        // }
+
+        switch(userId) {
+            case 20:
+                System.out.println("Welcome Admin" + " " + userId);
+                break;
+            case 21:
+                System.out.println("You can edit the files" + " " + userId);
+                break;
+            case 22:
+                System.out.println("You can view the files" + " " + userId);
+                break;
+            default:
+                System.out.println("You dont have access.");
+                
         }
-        if(userId == editorId) {
-            System.out.println("Welcome Editor");
-        }
-        if(userId == editorId || adminId == editorId) {
-            System.out.println("You can edit the files");
-        } else {
-            System.out.println("You dont have access");
-        }
+
     }
 }
 
