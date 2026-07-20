bf0e2d89e8804bbb412d3bbde31c8a1fb58d7a3e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-07-20 11:12:07 +0530

    Add CommentsInterface and VedioActionsInterface; implement comment methods in Paytm class

---
+++ PaymentsPractice/VedioActionsInterface.java
@@ -0,0 +1,9 @@
+package PaymentsPractice;
+
+interface VedioActionsInterface {
+    public void like();
+    public void dislike();
+    public void clip();
+    public void share();
+    public void download();
+}
